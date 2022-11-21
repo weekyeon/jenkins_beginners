961367413d426a76207592b58cbd846f0c7da970
weekyeon <weekyeon@gmail.com>
2022-11-21 13:52:47 +0900

    update HelloWorld.java

--- JavaProject/src/HelloWorld.java
+++ JavaProject/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("CI TEST");
+		System.out.println("너무 어렵잖아~");
 	}
 
 }
