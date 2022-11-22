6fce25ae4886c77d9be689d1d2e84e9282de0d29
weekyeon <weekyeon@gmail.com>
2022-11-22 16:52:21 +0900

    github webhook test

--- JavaProject/src/CompileTarget.java
+++ JavaProject/src/CompileTarget.java
@@ -4,6 +4,7 @@ public class CompileTarget {
 	public static void main(String[] args) {
 		System.out.println("Compile Target");
 		System.out.println("Check CI & Build");
+		System.out.println("Github Webhook........... 멀고도 가까운..");
 	}
 
 }
