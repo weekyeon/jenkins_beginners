9e81356c7a02f7ed57fdd7674163f94ec7dc513e
weekyeon <weekyeon@gmail.com>
2022-11-23 14:56:04 +0900

    jenkins webhook test online

--- JavaProject/src/CompileTarget.java
+++ JavaProject/src/CompileTarget.java
@@ -5,7 +5,7 @@ public class CompileTarget {
 		System.out.println("Compile Target");
 		System.out.println("Check CI & Build");
 		System.out.println("Github Webhook........... 멀고도 가까운..");
-		System.out.println("인터넷이 없어도 Webhook이 걸릴까? 'Web'hook인데..?");
+		System.out.println("지금은 인터넷이 있어!");
 	}
 
 }
